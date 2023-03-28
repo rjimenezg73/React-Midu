b84aff7754719c5faa2d99f5688f2caf09abe4c9
Roberto Jimenez <robertojimenezg@gmail.com>
2023-03-27 18:18:44 -0600

    Mostrando una serie de gifs almacenados en un arreglo por medio de useState

--- giffy/src/App.js
+++ giffy/src/App.js
@@ -1,8 +1,15 @@
 import React, {useState} from 'react';
 import './App.css';
+const GIFS = [
+  'https://media1.giphy.com/media/EPcvhM28ER9XW/giphy.gif?cid=ecf05e47iijq8c6dxkdr4j2o6xs9b9rdid092nv2zudm8hzi&rid=giphy.gif&ct=g',
+  'https://media4.giphy.com/media/pO1H8mAU7geAw/200w.webp?cid=ecf05e4714vu8eopegespxib73qjdnqxya7g8hw2of7at6e2&rid=200w.webp&ct=g',
+  'https://media1.giphy.com/media/cnbsOTkEJnq0/200w.webp?cid=ecf05e4714vu8eopegespxib73qjdnqxya7g8hw2of7at6e2&rid=200w.webp&ct=g',
+  'https://media4.giphy.com/media/Hs6f36KUBjWww/200w.webp?cid=ecf05e4714vu8eopegespxib73qjdnqxya7g8hw2of7at6e2&rid=200w.webp&ct=g',
+  'https://media0.giphy.com/media/EatwJZRUIv41G/giphy.webp?cid=ecf05e4714vu8eopegespxib73qjdnqxya7g8hw2of7at6e2&rid=giphy.webp&ct=g'
+]
 
 function App() {
-  const [gifs, setGifs] = useState(['https://media1.giphy.com/media/EPcvhM28ER9XW/giphy.gif?cid=ecf05e47iijq8c6dxkdr4j2o6xs9b9rdid092nv2zudm8hzi&rid=giphy.gif&ct=g']);
+  const [gifs, setGifs] = useState(GIFS);
   /* 
   O puede ser:
   const state = useState([]);
@@ -15,8 +22,10 @@ function App() {
       <section className="App-content">
         <h2>
           <p>G i f s</p>
-          <img src={gifs} />
         </h2>
+        {
+          gifs.map(singleGif => <img src={singleGif} />)
+        }
       </section>
     </div>
   );
